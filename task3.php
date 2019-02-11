88c255e2ce8f7f4eda6f63546f80227237bc4de3
GanulAnton <antonhanul96@gmail.com>
2019-02-11 16:12:32 +0300

    Задание 2.2(после презентации)

--- task3.php
+++ task3.php
@@ -7,12 +7,15 @@
         .active1 {
             background: #ffa69a;
         }
+
         .active2 {
             background: #ff806e;
         }
+
         .active3 {
             background: #ff3635;
         }
+
         .active4 {
             background: #ff0f00;
         }
@@ -20,46 +23,46 @@
     </style>
 </head>
 <body>
-<form name="authForm" method="GET" action="<?=$_SERVER['PHP_SELF']?>">
+<form name="authForm" method="GET" action="<?= $_SERVER['PHP_SELF'] ?>">
     n:<input type="text" name="n">
     <input type="submit">
 </form>
 <?php
 $n = $_GET['n'];
-echo '<table border="1">';
-echo'<tr><th>'.'Номер п/п'.'</th><th colspan= "1000">'.'Число'.'</th></tr>';
+echo '<table border="1"</br>>';
+echo '<tr><th>' . 'Номер п/п' . '</th><th colspan= "1000">' . 'Число' . '</th></tr>';
 
 echo '<tr class="active1">';
-if(!empty($n)) {
+if (!empty($n)) {
     echo '<td>' . '1' . '</td>';
 }
-for ($a=1; $a<=$n; $a++){
+for ($a = 1; $a <= $n; $a++) {
 
-    echo '<td>'.mt_rand(10,99). '</td>';
+    echo '<td>' . mt_rand(10, 99) . '</td>';
 }
 
 echo '<tr class="active2">';
-if(!empty($n)) {
+if (!empty($n)) {
     echo '<td>' . '2' . '</td>';
 }
-for ($a=1; $a<=$n; $a++){
-    echo '<td>'.mt_rand(10,99). '</td>';
+for ($a = 1; $a <= $n; $a++) {
+    echo '<td>' . mt_rand(10, 99) . '</td>';
 }
 
 echo '<tr class="active3">';
-if(!empty($n)) {
+if (!empty($n)) {
     echo '<td>' . '3' . '</td>';
 }
-for ($a=1; $a<=$n; $a++){
-    echo '<td>'.mt_rand(10,99). '</td>';
+for ($a = 1; $a <= $n; $a++) {
+    echo '<td>' . mt_rand(10, 99) . '</td>';
 }
 
 echo '<tr class="active4">';
-if(!empty($n)) {
+if (!empty($n)) {
     echo '<td>' . '4' . '</td>';
 }
-for ($a=1; $a<=$n; $a++){
-    echo '<td>'.mt_rand(10,99). '</td>';
+for ($a = 1; $a <= $n; $a++) {
+    echo '<td>' . mt_rand(10, 99) . '</td>';
 }
 
 echo '</table>';
